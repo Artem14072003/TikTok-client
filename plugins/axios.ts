c43bce35f89a54dc84629d8b625aa721f32fcbaf
Artem14072003 <artem_krivenko_2019@mail.ru>
2024-02-25 14:36:44 +0300

    Update to the server link

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default defineNuxtPlugin((NuxtApp) => {
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'https://tiktok-client.railway.internal'
+    axios.defaults.baseURL = 'https://tiktok-server-production.up.railway.app'
     return {
         provide: {
             axios: axios
